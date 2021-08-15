51be683ee1152975d550451a1f0328d6a5aae13e
RCoff <enfaduraa@gmail.com>
2021-08-15 12:00:41 -0400

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import logging
+
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
