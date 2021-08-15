6451ffe51ebe858746b52af95d330441eba3f55c
RCoff <enfaduraa@gmail.com>
2021-08-15 12:11:17 -0400

    Add authentication function

--- main.py
+++ main.py
@@ -8,7 +8,34 @@ from google.oauth2.credentials import Credentials
 
 
 def main():
-    pass
+    service_client = auth_google()
+
+
+def auth_google():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    return build('gmail', 'v1', credentials=creds)
 
 
 if __name__ == "__main__":
