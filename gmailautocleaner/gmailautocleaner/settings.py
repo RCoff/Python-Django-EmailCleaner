fe5b8c2b98252b1a32d3a8bc0cddcf34089df29d
RCoff <enfaduraa@gmail.com>
2021-08-15 21:51:17 -0400

    Update installed apps

--- gmailautocleaner/gmailautocleaner/settings.py
+++ gmailautocleaner/gmailautocleaner/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'interface',
+    'data',
+    'api',
+    'gmail_clean',
 ]
 
 MIDDLEWARE = [
