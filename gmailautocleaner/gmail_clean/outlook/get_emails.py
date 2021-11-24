a171aaa42aea3299c4fb0659a7efb9236e3ee8c6
RCoff <enfaduraa@gmail.com>
2021-11-23 21:29:19 -0500

    Implement function for getting emails from outlook

--- gmailautocleaner/gmail_clean/outlook/get_emails.py
+++ gmailautocleaner/gmail_clean/outlook/get_emails.py
@@ -1,6 +1,13 @@
-from django.shortcuts import HttpResponseRedirect, reverse
-from .auth import outlook_sign_in, get_token_from_code
+import logging
+
 import requests
+from django.shortcuts import HttpResponseRedirect, reverse
+
+from .auth import outlook_sign_in, get_token_from_code
+from data.models import EmailStorage
+from data.utils import write_messages_to_db
+
+logger = logging.getLogger(__name__)
 
 
 def get_emails(request):
@@ -8,17 +15,43 @@ def get_emails(request):
     token = get_token_from_code(request)['access_token']
 
     headers = {'Authorization': f'Bearer {token}'}
+    user_email = _get_user_email(token)
 
-    raw_messages = []
-    messages_resp = requests.get(endpoint_url, headers=headers).json()
-    if messages_resp.get('value'):
-        raw_messages = raw_messages + messages_resp['value']
+    # if request.session.get('messages_id'):
+    #     user_data = EmailStorage.objects.get(id=request.session.get('messages_id'))
 
-    while messages_resp.get('@odata.nextLink'):
-        messages_resp = requests.get(messages_resp['@odata.nextLink'], headers=headers).json()
+    user_data = EmailStorage.objects.filter(user_email=user_email).order_by('-created')
+    if len(user_data) > 0:
+        raw_messages = user_data[0].messages_json
+    else:
+        raw_messages = []
+        messages_resp = requests.get(endpoint_url, headers=headers).json()
         if messages_resp.get('value'):
             raw_messages = raw_messages + messages_resp['value']
 
-    print("pause")
+        while messages_resp.get('@odata.nextLink'):
+            messages_resp = requests.get(messages_resp['@odata.nextLink'], headers=headers).json()
+            if messages_resp.get('value'):
+                raw_messages = raw_messages + messages_resp['value']
+
+        print(f"{len(raw_messages)} messages returned")
+        new_messages = write_messages_to_db(user_email, {'messages': raw_messages})
+        request.session['messages_id'] = new_messages.id
 
     return raw_messages
+
+
+def _get_user_email(token: str) -> str:
+    headers = {'Authorization': f"Bearer {token}"}
+    endpoint_url = "https://graph.microsoft.com/v1.0/me?$select=userPrincipalName,email"
+
+    user_response = requests.get(endpoint_url, headers=headers)
+    if user_response.ok:
+        user_response = user_response.json()
+
+    if user_response.get('email'):
+        return user_response['email']
+    elif user_response.get('userPrincipalName'):
+        return user_response['userPrincipalName']
+    else:
+        raise ValueError("User email address not found")
