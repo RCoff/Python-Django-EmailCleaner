98f3ddf51c5c10340c01a7837350c6a12295dd16
RCoff <enfaduraa@gmail.com>
2021-12-31 16:41:54 -0500

    Split index into index and email files

--- gmailautocleaner/gmail_clean/email_views.py
+++ gmailautocleaner/gmail_clean/email_views.py
@@ -6,10 +6,10 @@ from gmail_clean.outlook import get_emails
 def load_gmail(request):
     if request.method == 'GET':
         loaded_emails = main.main(request.session)
-        return render(request, 'index.html', context={'loaded_emails': loaded_emails})
+        return render(request, 'emails.html', context={'loaded_emails': loaded_emails})
 
 
 def load_outlook(request):
     if request.method == 'GET':
         loaded_emails = get_emails(request)
-        return render(request, 'index.html', context={'loaded_emails': loaded_emails})
+        return render(request, 'emails.html', context={'loaded_emails': loaded_emails})
