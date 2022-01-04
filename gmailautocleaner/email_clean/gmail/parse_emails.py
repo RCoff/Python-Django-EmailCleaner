8e49a53f7f64826362e4bd75b858092ad99fb8f8
RCoff <enfaduraa@gmail.com>
2022-01-03 23:24:49 -0500

    Implement batch retrieval of individual messages

--- gmailautocleaner/email_clean/gmail/parse_emails.py
+++ gmailautocleaner/email_clean/gmail/parse_emails.py
@@ -1,7 +1,43 @@
+import datetime
 import logging
+import time
+
 from email_clean.utils import parse_email_domain
+from email_clean.exceptions import *
 
 logger = logging.getLogger(__name__)
+
+
+def _get_all_messages(messages: list, service_client) -> tuple:
+    message_detail_list = []
+
+    def _callback_get_all_messages(request_id, response, exception):
+        if exception:
+            if exception.status_code == 403:
+                raise QuotaLimitReached
+            else:
+                raise ValueError(exception)
+        else:
+            return message_detail_list.append(response)
+
+    chunk_size = 100
+    for chunk in range(0, len(messages), chunk_size):
+        message_batch = service_client.new_batch_http_request(callback=_callback_get_all_messages)
+        messages_chunk = messages[chunk: chunk + chunk_size]
+        for message in messages_chunk:
+            message_batch.add(service_client.users().messages().get(userId='me',
+                                                                    id=message['id'],
+                                                                    format='metadata'))
+        for retry in range(3):
+            try:
+                message_batch.execute()
+                break
+            except QuotaLimitReached:  # If quota limit is reached, wait and retry the batch
+                print(f"Retrying... {retry + 1}")  # TODO: Replace with logging
+                time.sleep(30 + (retry * 20))
+                continue
+
+    return tuple(message_detail_list)
 
 
 def parse_emails(messages: list, service_client) -> list:
@@ -13,11 +49,13 @@ def parse_emails(messages: list, service_client) -> list:
     """
 
     message_details_list = []
-    for message in messages:
-        message_details = service_client.users().messages().get(userId='me',
-                                                                id=message['id'],
-                                                                format='metadata').execute()
 
+    start_time = datetime.datetime.now()
+    message_response_tuple = _get_all_messages(messages, service_client)
+    print(f"Time to retrieve messages: {datetime.datetime.now() - start_time}")
+
+    start_time = datetime.datetime.now()
+    for message_details in message_response_tuple:
         message_detail_dict = {'id': message_details['id']}
 
         if 'headers' in message_details['payload']:
@@ -61,4 +99,5 @@ def parse_emails(messages: list, service_client) -> list:
 
     logging.debug(f"{len(message_details_list)} emails parsed")
 
+    print(f"Time to parse: {datetime.datetime.now() - start_time}")
     return message_details_list
