5eb695add716c002d1ad76047b5162b0e39d0084
RCoff <enfaduraa@gmail.com>
2022-01-09 11:45:39 -0500

    Add management command to clear expired results

---
+++ gmailautocleaner/data/management/commands/clear_expired_messages.py
@@ -0,0 +1,20 @@
+import datetime
+
+from django.core.management.base import BaseCommand, CommandError
+from django.utils import timezone
+from data.models import EmailStorage
+
+
+class Command(BaseCommand):
+    help = 'Delete user messages after 24 hours'
+
+    def handle(self, *args, **options):
+        dt = timezone.now()
+        expired_messages = EmailStorage.objects.filter(expiration__lt=dt)
+
+        for user_messages in expired_messages:
+            user_messages.raw_emails = None
+            user_messages.parsed_emails = None
+            user_messages.expiration = None
+            user_messages.save()
+
