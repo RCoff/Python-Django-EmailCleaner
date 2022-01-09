f6f2c815df63c5487dd575ffbafbf5461b1a6321
RCoff <enfaduraa@gmail.com>
2022-01-09 11:49:38 -0500

    Update EmailStorage model to only allow a single entry per user

--- gmailautocleaner/data/admin.py
+++ gmailautocleaner/data/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from data.models import EmailStorage
 
 # Register your models here.
+admin.site.register(EmailStorage)
--- gmailautocleaner/data/models.py
+++ gmailautocleaner/data/models.py
@@ -5,8 +5,8 @@ import uuid
 # Create your models here.
 class EmailStorage(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    user_email = models.EmailField()
-    raw_emails = models.JSONField()
-    parsed_emails = models.JSONField()
+    user_email = models.EmailField(unique=True, editable=False)
+    raw_emails = models.JSONField(null=True, blank=True)
+    parsed_emails = models.JSONField(null=True, blank=True)
     expiration = models.DateTimeField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True, editable=False)
