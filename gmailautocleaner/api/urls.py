77bb837b73686239113172f60562bdbc1887d930
RCoff <enfaduraa@gmail.com>
2022-01-10 21:59:21 -0500

    Modify task status route to better reflect what the endpoint returns

--- gmailautocleaner/api/urls.py
+++ gmailautocleaner/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('task/<uuid:task_id>', views.CeleryTaskResult.as_view(), name='task-status'),
+    path('task/<uuid:task_id>/status', views.CeleryTaskResult.as_view(), name='task-status'),
 ]
