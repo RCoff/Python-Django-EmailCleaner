432513d56d53071379a4e0f41a1d48e9f7955024
RCoff <enfaduraa@gmail.com>
2022-01-10 19:08:31 -0500

    Add logging

--- gmailautocleaner/data/management/commands/clear_expired_messages.py
+++ gmailautocleaner/data/management/commands/clear_expired_messages.py
@@ -1,8 +1,10 @@
-import datetime
+import logging
 
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from data.models import EmailStorage
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -12,10 +14,10 @@ class Command(BaseCommand):
         dt = timezone.now()
         expired_messages = EmailStorage.objects.filter(expiration__lt=dt)
 
+        logger.debug(f"{len(expired_messages)} user messages to clear")
         for user_messages in expired_messages:
             user_messages.raw_emails = None
             user_messages.parsed_emails = None
             user_messages.expiration = None
             user_messages.parse_status = 'ns'
             user_messages.save()
-
