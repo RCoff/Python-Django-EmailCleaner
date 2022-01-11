5590b7f0523efbbc85acabd550f62e963416d7fe
RCoff <enfaduraa@gmail.com>
2022-01-10 22:55:26 -0500

    Add view to sign out and forget credentials

--- gmailautocleaner/email_clean/auth_urls.py
+++ gmailautocleaner/email_clean/auth_urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 import email_clean.outlook.auth as outlook_auth
 import email_clean.gmail.auth as gmail_auth
+from .views import sign_out
 
 urlpatterns = [
+    path('sign-out/', sign_out, name='sign-out'),
     path('outlook/callback', outlook_auth.outlook_callback, name='outlook-callback'),
     path('outlook/sign-in', outlook_auth.outlook_sign_in, name='outlook-sign-in'),
     path('gmail/callback', gmail_auth.gmail_callback, name='gmail-callback'),
--- gmailautocleaner/email_clean/views.py
+++ gmailautocleaner/email_clean/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, HttpResponseRedirect
 from email_clean.gmail import main
 from email_clean.outlook import get_emails
 
@@ -12,3 +12,8 @@ def load_outlook(request):
     if request.method == 'GET':
         loaded_emails = get_emails(request)
         return render(request, 'emails.html', context={'loaded_emails': loaded_emails})
+
+
+def sign_out(request):
+    del request.session['credentials']
+    return HttpResponseRedirect(reverse('index'))
