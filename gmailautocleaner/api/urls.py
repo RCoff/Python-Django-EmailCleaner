773ee228893e9253ca4f16f2e9789431d25db5e9
RCoff <enfaduraa@gmail.com>
2022-01-13 19:12:31 -0500

    Add view for checking celery task queue

--- gmailautocleaner/api/urls.py
+++ gmailautocleaner/api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('task/<uuid:task_id>/status', views.CeleryTaskResult.as_view(), name='task-status'),
+    path('task/inspect', views.CeleryTaskInspect.as_view(), name='task-inspect'),
 ]
--- gmailautocleaner/api/views.py
+++ gmailautocleaner/api/views.py
@@ -5,7 +5,25 @@ from gmailautocleaner.celery import app
 
 
 # Create your views here.
-class CeleryTaskResult(View):
+class CeleryTaskInspectBase:
+    app_inspect = app.control.inspect()
+
+    def scheduled(self):
+        return self.app_inspect.scheduled()
+
+    def active(self):
+        return self.app_inspect.active()
+
+    def reserved(self):
+        return self.app_inspect.reserved()
+
+    def inspect_dict(self):
+        return {'scheduled': len(self.scheduled()),
+                'active': len(self.active()),
+                'reserved': len(self.reserved())}
+
+
+class CeleryTaskResult(View, CeleryTaskInspectBase):
     task_id = None
 
     def get(self, request, task_id: str):
@@ -13,3 +31,9 @@ class CeleryTaskResult(View):
         result = app.AsyncResult(str(task_id))
 
         return JsonResponse({'result': result.ready()})
+
+
+class CeleryTaskInspect(View, CeleryTaskInspectBase):
+
+    def get(self, request):
+        return JsonResponse(self.inspect_dict())
